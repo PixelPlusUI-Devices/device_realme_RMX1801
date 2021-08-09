5bd20993722435472ae45db66f9acd42a636db9a
allworkdone <raj635321@gmail.com>
2021-08-09 12:00:48 +0530

    Revert "RMX1801: Add overlay for masking the notch"
    This reverts commit a5b0dc35c7b4a497355297ab560cfe41bdc209b1.

--- device.mk
+++ device.mk
@@ -32,9 +32,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-aosp
 
-PRODUCT_PACKAGES += \
-    NoCutoutOverlay
-
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-aosp
