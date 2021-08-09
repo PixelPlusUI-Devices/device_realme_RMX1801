13cd82519dd548d3fc613243f0e1ce80c26fbefe
allworkdone <raj635321@gmail.com>
2021-08-09 12:08:34 +0530

    RMX1801: PixelPlusUI BringUP

--- aosp_RMX1801.mk
+++ aosp_RMX1801.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from Realme RMX1801
 $(call inherit-product, device/realme/RMX1801/device.mk)
@@ -27,7 +27,7 @@ $(call inherit-product, device/realme/RMX1801/device.mk)
 # Set Shipping API level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-PRODUCT_NAME := bliss_RMX1801
+PRODUCT_NAME := aosp_RMX1801
 PRODUCT_DEVICE := RMX1801
 PRODUCT_MANUFACTURER := OPPO
 PRODUCT_BRAND := oppo
@@ -42,12 +42,8 @@ TARGET_VENDOR_DEVICE_NAME := RMX1801
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="RMX1801"
 
+#PixelPlusUI specific Flags
 TARGET_FACE_UNLOCK_SUPPORTED := true
-
-#Bliss Build-Type
-BLISS_BUILD_VARIANT=vanilla
-
-#Now Official
-BLISS_BUILDTYPE := OFFICIAL
+CUSTOM_BUILD_TYPE := OFFICIAL
 
 TARGET_BOOT_ANIMATION_RES := 1080
