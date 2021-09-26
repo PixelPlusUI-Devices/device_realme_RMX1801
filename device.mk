6c35c1b57913245a31d03ae78820dc5254495c13
jro1979oliver <jroliveira.oliveira301@gmail.com>
2021-09-26 19:16:03 +0100

    RMX1801: Downrev audio to 4.0
    * Audio v5 has issues related with setMicrophoneFieldDimension, killing audio input causing
      signal 11 (SIGSEGV), code 1 (SEGV_MAPERR), fault addr 0x0
    
    Change-Id: Iff802131e67ca6cb507f5a3d3d850ad153288b83

--- device.mk
+++ device.mk
@@ -100,8 +100,8 @@ PRODUCT_COPY_FILES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
-    android.hardware.audio@5.0-impl \
-    android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio@4.0-impl \
+    android.hardware.audio.effect@4.0-impl \
     android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
     audio.r_submix.default \
