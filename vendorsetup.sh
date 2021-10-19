1149a2df76e0b29a04b35b54acc7b59346a4d4b2
allworkdone <raj635321@gmail.com>
2021-10-19 21:27:27 +0530

    RMX1801: vendorsetup.sh RealmeDirac

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+#Removing existing Realme Dirac
+rm -rf packages/apps/RealmeDirac > /dev/null 2>&1
+
+#Clone RealmeDirac
+git clone https://github.com/techyminati/android_packages_apps_RealmeDirac packages/apps/RealmeDirac
